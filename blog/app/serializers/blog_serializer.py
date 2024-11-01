7e59debdbb48cea596bc7ae1212e7c81597ae352
smashcodedev <65023462+waqasansarii@users.noreply.github.com>
2024-11-02 00:58:00 +0500

    added transaction

--- blog/app/serializers/blog_serializer.py
+++ blog/app/serializers/blog_serializer.py
@@ -1,3 +1,5 @@
+from django.db import transaction
+from rest_framework.response import Response
 from rest_framework import serializers
 from ..models import Blog,Category,User,Comments,BlogLike
 from .user_serializer import UserSerializer
@@ -49,19 +51,25 @@ class BlogSerializer(serializers.ModelSerializer):
     
     # override create method to add category and contributors in intermediate table 
     def create(self, validated_data):
-        contributors = validated_data.pop('contributors_id')
-        category = validated_data.pop('category_id')
-        validated_data['author'] = self.context['request'].user
-        blog = Blog.objects.create(**validated_data)
-        blog.contributors.set(contributors)
-        blog.category.set(category)
+        try:
+            with transaction.atomic():
+                contributors = validated_data.pop('contributors_id')
+                category = validated_data.pop('category_id')
+                validated_data['author'] = self.context['request'].user
+                blog = Blog.objects.create(**validated_data)
+                blog.contributors.set(contributors)
+                blog.category.set(category)
         
-        return blog  
+            return blog  
+        except Exception as e:
+            return e
+        # Response({'status': 'error', 'message': str(e)}, status=400)
+
     
     def update(self, instance, validated_data):
         instance.title = validated_data.get('title', instance.title)
         instance.content = validated_data.get('content', instance.content)
-        print('validated data',validated_data)
+        # print('validated data',validated_data)
         
         if  len(validated_data['contributors_id'])>0:
             contributors = validated_data.pop('contributors_id')
